7af29b35faca2c308e6a298d641adf32e6d5b2c2
Joel <vextechmage@gmail.com>
2024-03-11 22:58:01 -0700

    Added new migration for validation changes

---
+++ FrybreadFusion/Data/Migrations/20240312024523_AddValidation.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FrybreadFusion.Data.Migrations
+{
+    public partial class AddValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "FullName",
+                keyValue: null,
+                column: "FullName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
